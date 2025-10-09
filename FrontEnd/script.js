b3115498b33068f0f26f54d85a15581ba6158d70
lorenzosuet <lorenzo.suet@gmail.com>
2025-10-09 14:04:49 +0200

    Ajout des modifications dans script.js

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -4,22 +4,54 @@
 			</figure>*/
 
 // URL de ton API (ici un exemple)
-const apiUrl = "http://localhost:5678/api/works";
+const apiUrlWorks = "http://localhost:5678/api/works";
 
-fetchData() ;
+fetchProjects() ;
+           
+// URL de ton API (ici un exemple)
+const apiUrlCategories = "http://localhost:5678/api/categories";
 
+async function fetchCategories() {
+  try {
+    const response = await fetch(apiUrlCategories);
+    const categories = await response.json();
+    console.log(categories); // Appelle la fonction pour afficher les boutons
+    displayCategories(categories);
+  } catch (error) {
+    console.error("Erreur lors de la récupération des données :", error);
+  }
+}
+// Fonction pour créer les boutons et les injecter dans le HTML
+function displayCategories(buttons) {
+  const container = document.querySelector(".categories");
+  container.innerHTML = ""; // On vide le conteneur avant d'ajouter les boutons
 
+  buttons.forEach(button => {
+    const btn = document.createElement("button");
+    btn.textContent = button.name; // Assure-toi que l'API renvoie bien un champ 'name'
+    container.appendChild(btn);
+  });
+}
 
+// Appel de la fonction pour récupérer les données
+fetchCategories();
+
+// Appel de la fonction avec les données statiques
+displayCategories(categories);
 // Fonction pour récupérer les données
-async function fetchData() {
+async function fetchProjects(buttons) {
     try {
-        const response = await fetch(apiUrl);
-        const data = await response.json();
-        displayCards(data); // Appelle la fonction pour afficher les cartes
+        const response = await fetch(apiUrlWorks);
+        const projects = await response.json();
+        displayCards(projects); // Appelle la fonction pour afficher les cartes
     } catch (error) {
         console.error("Erreur lors de la récupération des données :", error);
     }
 }
+
+
+// Appel de la fonction avec les données statiques
+displayCategories(categories);
 
 // Fonction pour créer les cartes et les injecter dans le HTML
 function displayCards(items) {
